c6cd40bbf314c3addccd7e649411532866569e73
SrSolo <97929454+SrSolo@users.noreply.github.com>
2023-07-23 17:30:41 -0700

    backend updated

---
+++ api/generate_plan.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const apiKey = process.env.API_KEY;
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const requestData = req.body;
+
+        try {
+            const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+                prompt: `You selected ${requestData.daysPerWeek} days per week for training. Your weekly mileage goal is ${requestData.weeklyMileage} miles, and your race goal is to run a ${requestData.raceGoals} in ${requestData.racingGoalTime}. Your training plan should be ${requestData.trainingPlanLength} weeks long.`,
+                max_tokens: 200,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${apiKey}`,
+                },
+            });
+
+            const trainingPlan = response.data.choices[0].text;
+
+            // Send the generated training plan back to the frontend
+            res.status(200).json({ trainingPlan });
+        } catch (error) {
+            console.error('Error:', error.message);
+            // Handle error (send an error response to the frontend, etc.)
+            res.status(500).json({ error: 'An error occurred while generating the training plan.' });
+        }
+    } else {
+        res.status(405).end(); // Method Not Allowed
+    }
+}
